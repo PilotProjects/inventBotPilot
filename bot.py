c55411bf0c945da67eb7abbfc38cbbeef5875a29
Coding Cat <32220838+qtulhu@users.noreply.github.com>
2019-12-27 15:15:20 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,16 +30,16 @@ def start(message):
 def name(message):
     bot.reply_to(message,message.text)
 
-#@server.route("/920710380:AAG8uT7mRjpMXDkY13v4OZyrxt2jMV0JE6Y", methods=['POST'])
-#def getMessage():
-#    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-#    return "!", 200
+@server.route("/920710380:AAG8uT7mRjpMXDkY13v4OZyrxt2jMV0JE6Y", methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
 
-@server.route("/")
-def webhook():
-	bot.remove_webhook()
-	bot.set_webhook(url="https://iventbot.herokuapp.com/920710380:AAG8uT7mRjpMXDkY13v4OZyrxt2jMV0JE6Y")
-	return "!", 200
+#@server.route("/")
+#def webhook():
+#	bot.remove_webhook()
+#	bot.set_webhook(url="https://iventbot.herokuapp.com/920710380:AAG8uT7mRjpMXDkY13v4OZyrxt2jMV0JE6Y")
+#	return "!", 200
 
 
 server.run(host="0.0.0.0", port=os.environ.get('PORT'))
